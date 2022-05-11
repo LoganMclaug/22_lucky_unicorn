16eb2086bab650d99260ce34133594a32cbc505c
mclaug.logan <mclaug.logan@awatapu.school.nz>
2022-05-12 10:20:50 +1200

    Created yes_no_checker function.

--- venv/02_yes_no_checker(V3).py
+++ venv/02_yes_no_checker(V3).py
@@ -4,17 +4,17 @@ def yes_no(question):
     while not valid:
 
         # ask user for input
-        response = input(question).lower()
+        response = input(question).lower().strip()
 
         # if input = "yes" or "y" program continues
         if response == "yes" or response == "y":
             response = "yes"
-            print("You chose yes")
+            return response
 
         # if input = "no" or "n" display instructions
         elif response == "no" or response == "n":
             response = "no"
-            print("You chose no")
+            return response
 
         # if input = invalid, re-ask question
         else:
