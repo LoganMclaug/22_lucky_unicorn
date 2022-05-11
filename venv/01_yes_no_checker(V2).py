8c173f1a0980857752b4d891fbc9970b5d991083
mclaug.logan <mclaug.logan@awatapu.school.nz>
2022-05-12 10:12:03 +1200

    Base build for yes_no_checker.

---
+++ venv/01_yes_no_checker(V2).py
@@ -0,0 +1,17 @@
+# start of loop
+show_instructions = "valid"
+while show_instructions != "0":
+    # ask user for input
+    show_instructions = input("Have you played the Lucky Unicorn Game before?").lower()
+
+    # if input = "yes" or "y" program continues
+    if show_instructions == "y" or show_instructions == "yes":
+        print("program continues")
+
+    # if input = "no" or "n" display instructions
+    elif show_instructions == "n" or show_instructions == "no":
+        print("display instructions")
+
+    # if input = invalid re-ask question
+    else:
+        print("Please enter yes or no!")
